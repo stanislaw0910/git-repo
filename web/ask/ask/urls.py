652da749ab21207bda0e5e2f03225e44b2eb4a68
Stanislaw0910 <stanislaw0910@gmail.com>
2019-05-16 16:24:31 +0300

    1

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -30,6 +30,6 @@ urlpatterns = [
     url (r'^signup/', include ('qa.urls')),
     url (r'^question/\d+/', include ('qa.urls')),
     url (r'^ask/', include ('qa.urls')),
-    url (r'^popular/', include ('qa.urls'),
-    url (r'^new/', include ('qa.urls'))
+    url (r'^popular/', include ('qa.urls')),
+    url (r'^new/', include ('qa.urls') ),
 ]
