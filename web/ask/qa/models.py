c18c045cfa56aa40782c1bf9533f2a571f5f6655
stanislaw0910 <stanislaw0910@gmail.com>
2019-06-11 18:04:33 +0000

    1

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
+
+class QuestionManager(models.Manager):
+    def popular(self):
+        return self.order_by('-rating')
+    def new(self):
+        return self.order_by('-added_at')
+
+
 class Question(models.Model):
     title = models.CharField(default='', max_length=255)
     text = models.TextField(default='')
@@ -9,28 +17,10 @@ class Question(models.Model):
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
     likes = models.ManyToManyField(User, related_name='q_likes' )
     objects = QuestionManager()
-class QuestionManager(Question.Manager):
-    def popular(self):
-        return self.order_by('-rating')
-    def new(self):
-        return self.order_by('-added_at')
+
 
 class Answer(models.Model):
     text = models.TextField(default='')
     added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
-'''Question - вопрос
-title - заголовок вопроса
-text - полный текст вопроса
-added_at - дата добавления вопроса
-rating - рейтинг вопроса (число)
-author - автор вопроса
-likes - список пользователей, поставивших "лайк"
-
-Answer - ответ
-text - текст ответа
-added_at - дата добавления ответа
-question - вопрос, к которому относится ответ
-author - автор ответа
-'''
