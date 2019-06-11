0838e045bb84980bae670e53a8e109456a14ebf1
Stanislaw0910 <stanislaw0910@gmail.com>
2019-06-11 12:38:52 +0300

    1

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
+sudo pip3 install Django==2.0.0
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn1.conf   /etc/gunicorn.d/wsgi.example
--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+    likes = models.ManyToManyField()
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
+'''Question - вопрос
+title - заголовок вопроса
+text - полный текст вопроса
+added_at - дата добавления вопроса
+rating - рейтинг вопроса (число)
+author - автор вопроса
+likes - список пользователей, поставивших "лайк"
+
+Answer - ответ
+text - текст ответа
+added_at - дата добавления ответа
+question - вопрос, к которому относится ответ
+author - автор ответа
+'''
